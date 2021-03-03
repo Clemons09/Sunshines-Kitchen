0e9424698358d5532e2dceb3a1271ba199cfb058
Cody Lemons <clemons9988@gmail.com>
2021-03-03 09:12:29 -0600

    Added various features and styles

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#fixings1").hide();
 
   $("#maincheck1").click(function() {
-    $("#fixings1").slideToggle("slow");
+    $("#fixings1").toggle("slow");
 
   });
 
@@ -19,4 +19,43 @@ $(document).ready(function() {
     $("#fixings3").slideToggle("slow");
 
   });
+  $("#fixings4").hide();
+
+  $("#maincheck4").click(function() {
+    $("#fixings4").slideToggle("slow");
+
+  });
+  $("#fixings5").hide();
+
+  $("#maincheck5").click(function() {
+    $("#fixings5").slideToggle("slow");
+
+  });
+  $("#fixings6").hide();
+
+  $("#maincheck6").click(function() {
+    $("#fixings6").slideToggle("slow");
+
+  });
+  $("#fixings7").hide();
+
+  $("#maincheck7").click(function() {
+    $("#fixings7").slideToggle("slow");
+
+  });
+  $("#fixings8").hide();
+
+  $("#maincheck8").click(function() {
+    $("#fixings8").slideToggle("slow");
+
+  });
+  $("#fixings9").hide();
+
+  $("#maincheck9").click(function() {
+    $("#fixings9").slideToggle("slow");
+
+  });
+    
 });
+
+
